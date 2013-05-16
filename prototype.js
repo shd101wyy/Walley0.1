618b45e51073ae097c33ddd18b82af1fb6987a91
shd101wyy <shd101wyy@sina.com>
2013-05-16 19:19:43 +0800

    Improve codes

---
+++ prototype.js
@@ -0,0 +1,45 @@
+// String
+// find
+String.prototype.find=function(find_str, start){
+    if(typeof(start)=='undefined'){start=0};
+    return this.indexOf(find_str,start);
+}
+// tolower
+String.prototype.tolower=function(){
+    return this.toLowerCase();
+}
+// toupper
+String.prototype.toupper=function(){
+    return this.toUpperCase();
+}
+// reverse
+String.prototype.reverse=function(){
+    return this.split("").reverse().join("");
+}
+
+// Math
+
+Math["cot"]=function(num){
+    return 1/Math.tan(num);
+}
+Math["sec"]=function(num){
+    return 1/Math.cos(num);
+}
+Math["csc"]=function(num){
+    return 1/Math.sin(num);
+}
+
+// Array
+Array.prototype.append=function(obj){
+    this.push(obj);
+}
+
+Array.prototype.insert=function(insert_value,at_index){
+    if(typeof(at_index)=="undefined"){return this.push(insert_value);};
+    this.splice(insert_value,0,at_index);
+}
+
+Array.prototype.remove=function(remove_at_index){
+    this.splice(remove_at_index,1);
+}
+
--- walley_cg_js.h
+++ walley_cg_js.h
@@ -766,6 +766,12 @@ Str_List *Compile_to_JS(char *file_name){
     Str_List *output_sl;
     SL_initSL(&output_sl);
     
+    // this string is from prototype.js in walley folder
+    char *preload_js_str="String.prototype.find=function(e,t){if(typeof t==\"undefined\"){t=0}return this.indexOf(e,t)};String.prototype.tolower=function(){return this.toLowerCase()};String.prototype.toupper=function(){return this.toUpperCase()};String.prototype.reverse=function(){return this.split("").reverse().join("")};Math[\"cot\"]=function(e){return 1/Math.tan(e)};Math[\"sec\"]=function(e){return 1/Math.cos(e)};Math[\"csc\"]=function(e){return 1/Math.sin(e)};Array.prototype.append=function(e){this.push(e)};Array.prototype.insert=function(e,t){if(typeof t==\"undefined\"){return this.push(e)}this.splice(e,0,t)};Array.prototype.remove=function(e){this.splice(e,1)}";
+    SL_addString(&output_sl, preload_js_str);
+    
+    
+    
     
     while (sl_in_file!=NULL) {
         
